d394c36e613c7497e35dcd85890844eacfbb0cf4
raviakella03 <raviakella03@outlook.com>
2022-11-08 23:01:28 +0530

    ClosestMinMaxImpl and ClosestMinMaxTest

--- src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/ClosestMinMaxImpl.java
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/ClosestMinMaxImpl.java
@@ -1,12 +1,64 @@
 package me.ravi.scaler.practice.serviceimpl.intermediatedsa;
 
 import me.ravi.scaler.practice.service.intermediatedsa.ClosestMinMax;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class ClosestMinMaxImpl implements ClosestMinMax {
     @Override
     public int solve(ArrayList<Integer> A) {
-        return 0;
+        int min = A.get(0);
+        int max = A.get(0);
+        int maxIndex = -1;
+        int minIndex = -1;
+        int minLength = A.size();
+
+        /**
+         * Finding min in the array
+         */
+        for (int i = 1; i < A.size(); i++) {
+            if (A.get(i) < min) {
+                min = A.get(i);
+            }
+        }
+        /**
+         * Finding max in the array
+         */
+        for (int i = 1; i < A.size(); i++) {
+            if (A.get(i) > max) {
+                max = A.get(i);
+            }
+        }
+        if (min == max) {
+            return 1;
+        }
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) == max) {
+                maxIndex = i;
+            } else if (A.get(i) == min) {
+                minIndex = i;
+            }
+
+            if (minIndex == -1 || maxIndex == -1) {
+                continue;
+            }
+
+            if (minLength == -1) {
+                minLength = Math.abs(minIndex - maxIndex) + 1;
+            } else if (minLength == 1) {
+                break;
+            } else if (minLength > Math.abs(minIndex - maxIndex) + 1) {
+                minLength = Math.abs(minIndex - maxIndex) + 1;
+            }
+
+            if (A.get(i) == max) {
+                minIndex = -1;
+            } else if (A.get(i) == min) {
+                maxIndex = -1;
+            }
+        }
+        return minLength;
     }
 }
---
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/ClosestMinMaxTest.java
@@ -0,0 +1,44 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ClosestMinMaxTest {
+
+    private ArrayList<Integer> A;
+    private int expectedOutput;
+
+    @Autowired
+    private ClosestMinMax closestMinMax;
+
+    @AfterEach
+    public void checkOutput() {
+        assertEquals(expectedOutput, closestMinMax.solve(A));
+    }
+
+    @Test
+    public void scenario1() {
+        A = new ArrayList<>(Arrays.asList(814, 761, 697, 483, 981));
+        expectedOutput = 2;
+    }
+
+    @Test
+    public void scenario2() {
+        A = new ArrayList<>(Arrays.asList(613, 116, 420, 672, 426));
+        expectedOutput = 3;
+    }
+
+    @Test
+    public void scenario3() {
+        A = new ArrayList<>(Arrays.asList(4, 4, 4, 4, 4));
+        expectedOutput = 1;
+    }
+}

1ca7f06dee0d7df0c8810d5a0925cd47d1043751
raviakella03 <raviakella03@outlook.com>
2022-11-09 01:04:14 +0530

    Changes made to RangeSumQuery

--- src/main/java/me/ravi/scaler/practice/service/intermediatedsa/RangeSumQuery.java
+++ src/main/java/me/ravi/scaler/practice/service/intermediatedsa/RangeSumQuery.java
@@ -3,7 +3,6 @@ package me.ravi.scaler.practice.service.intermediatedsa;
 import java.util.ArrayList;
 
 public interface RangeSumQuery {
-    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B);
-
+    
     public ArrayList<Long> rangeSum(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B);
 }
--- src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/RangeSumQueryImpl.java
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/RangeSumQueryImpl.java
@@ -5,19 +5,7 @@ import me.ravi.scaler.practice.service.intermediatedsa.RangeSumQuery;
 import java.util.ArrayList;
 
 public class RangeSumQueryImpl implements RangeSumQuery {
-    @Override
-    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
-        ArrayList<Integer> sumList = new ArrayList<>();
-        int leftIndex;
-        int rightIndex;
-        for (int i = 0; i < B.size(); i++) {
-            leftIndex = B.get(i).get(0) - 1;
-            rightIndex = B.get(i).get(1) - 1;
-            sumList.add(sumArrayRange(A, leftIndex, rightIndex));
-        }
-        return sumList;
-    }
-
+    
     @Override
     public ArrayList<Long> rangeSum(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
         ArrayList<Long> sumList = new ArrayList<>();
@@ -40,13 +28,5 @@ public class RangeSumQueryImpl implements RangeSumQuery {
         for (int i = 1; i < A.size(); i++) {
             prefixSumList.add(prefixSumList.get(i - 1) + A.get(i));
         }
-    }
-
-    private int sumArrayRange(ArrayList<Integer> A, int leftIndex, int rightIndex) {
-        int sum = 0;
-        for (int i = leftIndex; i <= rightIndex; i++) {
-            sum += A.get(i);
-        }
-        return sum;
     }
 }

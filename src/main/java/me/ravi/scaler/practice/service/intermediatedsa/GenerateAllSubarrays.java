e8d0127068ab20cbc97288edb23734564b3f9eef
raviakella03 <raviakella03@outlook.com>
2022-11-16 23:40:22 +0530

    GenerateAllSubarrays

---
+++ src/main/java/me/ravi/scaler/practice/service/intermediatedsa/GenerateAllSubarrays.java
@@ -0,0 +1,7 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import java.util.ArrayList;
+
+public interface GenerateAllSubarrays {
+    public ArrayList<ArrayList<Integer>> solve(ArrayList<Integer> A);
+}

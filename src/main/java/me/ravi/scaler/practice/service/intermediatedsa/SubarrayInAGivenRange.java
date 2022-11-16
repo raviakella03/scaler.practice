d8109b5623425d588e7655caaf8b613907a122cc
raviakella03 <raviakella03@outlook.com>
2022-11-16 23:37:41 +0530

    SubarrayInAGivenRange

---
+++ src/main/java/me/ravi/scaler/practice/service/intermediatedsa/SubarrayInAGivenRange.java
@@ -0,0 +1,7 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import java.util.ArrayList;
+
+public interface SubarrayInAGivenRange {
+    public ArrayList<Integer> solve(ArrayList<Integer> A, int B, int C);
+}

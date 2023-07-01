01e8a2faa299496ff0c3f85e2609541feb574e5b
raviakella03 <raviakella03@outlook.com>
2023-07-01 15:23:06 +0530

    Added output check for Row Sum test class

--- src/test/java/me/ravi/scaler/practice/service/intermediatedsa/RowSumTest.java
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/RowSumTest.java
@@ -1,5 +1,23 @@
 package me.ravi.scaler.practice.service.intermediatedsa;
 
-class RowSumTest {
+import me.ravi.scaler.practice.utils.AbstractTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+@SpringBootTest
+class RowSumTest extends AbstractTest {
+
+    @Autowired
+    RowSum rowSum;
+    ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+    ArrayList<Integer> expectedResult = new ArrayList<>();
+
+    @Override
+    public void checkOutput() {
+        assertIterableEquals(expectedResult, rowSum.solve(A));
+    }
 }

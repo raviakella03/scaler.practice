17488269365715fae7baa3a62d326130464c9ceb
raviakella03 <raviakella03@outlook.com>
2023-07-01 15:34:31 +0530

    Added test cases for Row Sum

--- src/test/java/me/ravi/scaler/practice/service/intermediatedsa/RowSumTest.java
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/RowSumTest.java
@@ -1,10 +1,12 @@
 package me.ravi.scaler.practice.service.intermediatedsa;
 
 import me.ravi.scaler.practice.utils.AbstractTest;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
@@ -14,10 +16,19 @@ class RowSumTest extends AbstractTest {
     @Autowired
     RowSum rowSum;
     ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-    ArrayList<Integer> expectedResult = new ArrayList<>();
+    ArrayList<Integer> expectedResult;
 
     @Override
     public void checkOutput() {
         assertIterableEquals(expectedResult, rowSum.solve(A));
+        A.clear();
+    }
+
+    @Test
+    public void scenario1() {
+        A.add(new ArrayList<>(Arrays.asList(1, 2, 3, 4)));
+        A.add(new ArrayList<>(Arrays.asList(5, 6, 7, 8)));
+        A.add(new ArrayList<>(Arrays.asList(9, 2, 3, 4)));
+        expectedResult = new ArrayList<>(Arrays.asList(10, 26, 18));
     }
 }

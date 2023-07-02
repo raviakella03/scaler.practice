54fd536970e5617ce602f0bc14f440d81ee78a67
raviakella03 <raviakella03@outlook.com>
2023-07-02 15:10:53 +0530

    Added interface for Main diagonal sum

---
+++ src/main/java/me/ravi/scaler/practice/service/intermediatedsa/MainDiagonalSum.java
@@ -0,0 +1,8 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface MainDiagonalSum {
+    public int solve(final List<ArrayList<Integer>> A);
+}

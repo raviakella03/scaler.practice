e61b0beea2e34de04e8ec2a2e17cfc979ae4d343
raviakella03 <raviakella03@outlook.com>
2023-11-04 15:33:55 +0530

    Adds new interface for Matrix Transpose Problem

---
+++ src/main/java/me/ravi/scaler/practice/service/intermediatedsa/MatrixTranspose.java
@@ -0,0 +1,9 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import java.util.ArrayList;
+
+public interface MatrixTranspose {
+    public ArrayList<ArrayList<Integer>> solve(ArrayList<ArrayList<Integer>> A);
+
+    public int[][] solve(int[][] A);
+}

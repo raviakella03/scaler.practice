3586994f00ed4fd5881b164fe39e7e25466e7c7b
raviakella03 <raviakella03@outlook.com>
2023-11-13 10:48:47 +0530

    NumberOfOneBitsTest test cases

--- src/test/java/me/ravi/scaler/practice/service/intermediatedsa/NumberOfOneBitsTest.java
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/NumberOfOneBitsTest.java
@@ -2,10 +2,19 @@ package me.ravi.scaler.practice.service.intermediatedsa;
 
 import me.ravi.scaler.practice.utils.AbstractTest;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class NumberOfOneBitsTest extends AbstractTest {
+
+    @Autowired
+    NumberOfOneBits numberOfOneBits;
+    int A;
+    int expectedOutput;
 
     /**
      *
@@ -13,8 +22,24 @@ class NumberOfOneBitsTest extends AbstractTest {
     @Override
     @AfterEach
     public void checkOutput() {
-        
+        assertEquals(expectedOutput, numberOfOneBits.numSetBits(A));
     }
 
+    @Test
+    public void scenario1() {
+        A = 11;
+        expectedOutput = 3;
+    }
 
+    @Test
+    public void scenario2() {
+        A = 6;
+        expectedOutput = 2;
+    }
+
+    @Test
+    public void scenario3() {
+        A = 0;
+        expectedOutput = 0;
+    }
 }

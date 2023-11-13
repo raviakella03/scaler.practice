4249314299de5ee66aeb9eaf5ead7164a8460cdd
raviakella03 <raviakella03@outlook.com>
2023-11-13 10:48:00 +0530

    NumberOfOneBitsTest

---
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/NumberOfOneBitsTest.java
@@ -0,0 +1,20 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import me.ravi.scaler.practice.utils.AbstractTest;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberOfOneBitsTest extends AbstractTest {
+
+    /**
+     *
+     */
+    @Override
+    @AfterEach
+    public void checkOutput() {
+        
+    }
+
+
+}

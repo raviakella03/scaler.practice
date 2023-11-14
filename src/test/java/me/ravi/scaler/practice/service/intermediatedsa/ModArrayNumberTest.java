cdbf58f332ad2281a581db23debae9216be2441d
raviakella03 <raviakella03@outlook.com>
2023-11-15 01:46:57 +0530

    ModArrayNumberTest

---
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/ModArrayNumberTest.java
@@ -0,0 +1,9 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import me.ravi.scaler.practice.utils.AbstractTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModArrayNumberTest extends AbstractTest {
+
+}

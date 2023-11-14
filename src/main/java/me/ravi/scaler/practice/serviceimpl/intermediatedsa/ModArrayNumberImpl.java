ab1ba3460a8808acff9705bac49b8b0c6c202a3c
raviakella03 <raviakella03@outlook.com>
2023-11-15 01:48:54 +0530

    ModArrayNumberImpl

--- src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/ModArrayNumberImpl.java
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/ModArrayNumberImpl.java
@@ -14,6 +14,13 @@ public class ModArrayNumberImpl implements ModArrayNumber {
      */
     @Override
     public int solve(ArrayList<Integer> A, int B) {
-        
+        int sizeOfArray = A.size();
+        long number = 0;
+        long tenPowerContribution = 1;
+        for (int i = sizeOfArray - 1; i >= 0; i--) {
+            number = (number + A.get(i) * tenPowerContribution) % B;
+            tenPowerContribution = (tenPowerContribution * 10) % B;
+        }
+        return (int) number;
     }
 }

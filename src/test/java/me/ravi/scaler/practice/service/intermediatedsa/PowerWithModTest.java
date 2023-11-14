1da012bf798ad86faa792f240f3f6497a6ad4120
raviakella03 <raviakella03@outlook.com>
2023-11-14 17:57:15 +0530

    PowerWithModTest

---
+++ src/test/java/me/ravi/scaler/practice/service/intermediatedsa/PowerWithModTest.java
@@ -0,0 +1,10 @@
+package me.ravi.scaler.practice.service.intermediatedsa;
+
+import me.ravi.scaler.practice.utils.AbstractTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerWithModTest extends AbstractTest {
+
+    
+}

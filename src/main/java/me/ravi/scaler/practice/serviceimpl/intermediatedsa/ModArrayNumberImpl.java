9db0c38fc8a1b9d0908dab40100690cdf1022e92
raviakella03 <raviakella03@outlook.com>
2023-11-15 01:48:27 +0530

    ModArrayNumberImpl

---
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/intermediatedsa/ModArrayNumberImpl.java
@@ -0,0 +1,19 @@
+package me.ravi.scaler.practice.serviceimpl.intermediatedsa;
+
+import me.ravi.scaler.practice.service.intermediatedsa.ModArrayNumber;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class ModArrayNumberImpl implements ModArrayNumber {
+    /**
+     * @param A
+     * @param B
+     * @return
+     */
+    @Override
+    public int solve(ArrayList<Integer> A, int B) {
+        
+    }
+}

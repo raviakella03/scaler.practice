34aba40a60783aab90c454b157261509bc76428f
raviakella03 <raviakella03@outlook.com>
2023-11-16 22:04:32 +0530

    ElementsRemovalImpl

---
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/advanceddsa/adsa1/ElementsRemovalImpl.java
@@ -0,0 +1,7 @@
+package me.ravi.scaler.practice.serviceimpl.advanceddsa.adsa1;
+
+import me.ravi.scaler.practice.service.intermediatedsa.ElementsRemoval;
+
+public class ElementsRemovalImpl implements ElementsRemoval {
+    
+}

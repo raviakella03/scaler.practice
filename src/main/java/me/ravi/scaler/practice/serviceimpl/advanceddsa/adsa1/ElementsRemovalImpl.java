8a4b410e29c7b8c809ad175e1fee7dc442e4122d
raviakella03 <raviakella03@outlook.com>
2023-11-16 22:05:28 +0530

    ElementsRemovalImpl

--- src/main/java/me/ravi/scaler/practice/serviceimpl/advanceddsa/adsa1/ElementsRemovalImpl.java
+++ src/main/java/me/ravi/scaler/practice/serviceimpl/advanceddsa/adsa1/ElementsRemovalImpl.java
@@ -2,6 +2,22 @@ package me.ravi.scaler.practice.serviceimpl.advanceddsa.adsa1;
 
 import me.ravi.scaler.practice.service.intermediatedsa.ElementsRemoval;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ElementsRemovalImpl implements ElementsRemoval {
-    
+    /**
+     * @param A
+     * @return
+     */
+    @Override
+    public int solve(ArrayList<Integer> A) {
+        int sizeOfArrayList = A.size();
+        int finalCost = 0;
+        Collections.sort(A);
+        for (int i = 0; i < sizeOfArrayList; i++) {
+            finalCost = finalCost + A.get(i) * (sizeOfArrayList - i);
+        }
+        return finalCost;
+    }
 }
